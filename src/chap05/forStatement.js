a26760dfe4c4970dde6699f2610e6d05ac9bbacc
yuyahy <xvto.ya@gmail.com>
2024-10-31 20:35:23 +0900

    feat: chap05完了 (#3)

---
+++ src/chap05/forStatement.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var ans = 0;
+for (var i = 1; i <= 100; i++) {
+    ans += i;
+}
+console.log("\u7D50\u679C: ".concat(ans));
